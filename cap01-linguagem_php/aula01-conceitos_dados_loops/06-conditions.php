db8ad0dd38602779d3307ea62e6cdb636b738c8c
Osvaldo Alves <oalvesxp@gmail.com>
2023-11-28 13:32:30 -0300

    Adding exercise

--- cap01-linguagem_php/aula01-conceitos_dados_loops/06-conditions.php
+++ cap01-linguagem_php/aula01-conceitos_dados_loops/06-conditions.php
@@ -11,9 +11,9 @@
  $idade = 25;
  
  if ($idade > 18) {
-     echo "Você é maior de idade.";
+     echo "Você é maior de idade." . PHP_EOL;
  } else {
-     echo "Você é menor de idade.";
+     echo "Você é menor de idade." . PHP_EOL;
  }
 
  // Verifica se os valores são iguais: Igualdade (==)
@@ -21,9 +21,9 @@
  $b = "5";
  
  if ($a == $b) {
-     echo "Os valores são iguais.";
+     echo "Os valores são iguais." . PHP_EOL;
  } else {
-     echo "Os valores são diferentes.";
+     echo "Os valores são diferentes." . PHP_EOL;
  }
 
  // Veifica se os valores são iguais e do mesmo tipo: Igualdade Estrita (===)
@@ -31,9 +31,9 @@
  $b = "5";
  
  if ($a === $b) {
-     echo "Os valores são iguais e do mesmo tipo.";
+     echo "Os valores são iguais e do mesmo tipo." . PHP_EOL;
  } else {
-     echo "Os valores são diferentes ou do tipo diferente.";
+     echo "Os valores são diferentes ou do tipo diferente." . PHP_EOL;
  }
 
  // Verifica se dois valores são diferentes: Desigualdade (!= ou <>)
@@ -41,9 +41,9 @@
  $y = 5;
  
  if ($x != $y) {
-     echo "Os valores são diferentes.";
+     echo "Os valores são diferentes." . PHP_EOL;
  } else {
-     echo "Os valores são iguais.";
+     echo "Os valores são iguais." . PHP_EOL;
  }
 
  // Verifica se dois valores são diferentes e do mesmo tipo: Desigualdade Estrita (!==)
@@ -51,16 +51,29 @@
  $y = "10";
 
  if ($x !== $y) {
-     echo "Os valores são diferentes ou do tipo diferente.";
+     echo "Os valores são diferentes ou do tipo diferente." . PHP_EOL;
  } else {
-     echo "Os valores são iguais e do mesmo tipo.";
+     echo "Os valores são iguais e do mesmo tipo." . PHP_EOL;
  }
 
  // Permitem combinar condições: Operadores Lógicos (&&, ||, !)
  $nota = 75;
  
  if ($nota >= 70 && $nota <= 100) {
-     echo "Você passou no exame.";
+     echo "Você passou no exame." . PHP_EOL;
  } else {
-     echo "Você não passou no exame.";
+     echo "Você não passou no exame." . PHP_EOL;
+ }
+
+ echo "\n";
+ // Exercício
+ // Permite fazer uma condição composta: IF, ELSEIF e ELSE
+ $nota = 4.5;
+
+ if ($nota >= 8) {
+     echo "Parabéns! Você passou de ano direto." . PHP_EOL;
+ } elseif ($nota >= 5 && $nota <= 7.5) {
+     echo "Você está de recuperação. Faça o exercício de recuperação." . PHP_EOL;
+ } else {
+     echo "Infelizmente, você reprovou na matéria." . PHP_EOL;
  }
