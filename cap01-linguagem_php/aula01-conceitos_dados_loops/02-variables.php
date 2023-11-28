13ac84e436869feae48aa03e2d04f2e34d2a7a1c
Osvaldo Alves <oalvesxp@gmail.com>
2023-11-28 12:28:28 -0300

    first variables

---
+++ cap01-linguagem_php/aula01-conceitos_dados_loops/02-variables.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by VisualStudioCode.
+ * User: oalves
+ * Date: 28/11/2023
+ * Description: Manipulando variáveis em PHP.
+ */
+
+ $nome = 'Osvaldo Alves';
+ $idade = 21;
+
+echo "Eu sou $nome, e tenho $idade anos!";
+ 
